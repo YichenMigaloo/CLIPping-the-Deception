6738f88d639994a7ac0762976fdef9fc3f28bd8b
YichenMigaloo <98346342+YichenMigaloo@users.noreply.github.com>
2024-08-31 18:11:56 -0400

    Update clip_adapter.py

--- trainers/clip_adapter.py
+++ trainers/clip_adapter.py
@@ -84,10 +84,7 @@ class Adapter(nn.Module):
     def __init__(self, c_in, reduction=4):
         super(Adapter, self).__init__()
         self.fc = nn.Sequential(
-            nn.Linear(768, 384, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(384, 768, bias=False),
-            nn.ReLU(inplace=True),
+            
             nn.Linear(768, 384, bias=False),
             nn.ReLU(inplace=True),
             nn.Linear(384, 768, bias=False),
