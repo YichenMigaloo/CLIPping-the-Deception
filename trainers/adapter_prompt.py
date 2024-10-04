bcef2a73d3ce30913aa60b25a1b2b09c45f3be45
YichenMigaloo <98346342+YichenMigaloo@users.noreply.github.com>
2024-10-04 16:12:34 -0400

    Update adapter_prompt.py

--- trainers/adapter_prompt.py
+++ trainers/adapter_prompt.py
@@ -260,9 +260,9 @@ class UnifiedTrainer(TrainerX):
         self.sched = build_lr_scheduler(self.optim, cfg.OPTIM)
 
         # Manual registration of model and optimizer
-        self._models["UnifiedTrainer"] = self.model
-        self._optims["UnifiedTrainer"] = self.optim
-        self._scheds["UnifiedTrainer"] = self.sched
+        self._models["unifiedtrainer"] = self.model
+        self._optims["unifiedtrainer"] = self.optim
+        self._scheds["unifiedtrainer"] = self.sched
 
         if torch.cuda.device_count() > 1:
             print(f"Multiple GPUs detected ({torch.cuda.device_count()} GPUs), using DataParallel")
